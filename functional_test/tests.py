4bd7cf4424b1c1791b949217e7b9e59fa00f3983
nconnel4 <nconnel4@hotmail.com>
2023-03-20 16:42:51 -0400

    Add bootstrap styling

--- functional_test/tests.py
+++ functional_test/tests.py
@@ -1,6 +1,6 @@
 import time
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
@@ -8,7 +8,7 @@ from selenium.webdriver.common.keys import Keys
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self) -> None:
         self.browser = webdriver.Chrome()
 
@@ -113,19 +113,18 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
 
-        # She notices the input box i nicely centered
+        # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id("id_new_item")
         self.assertAlmostEqual(
-            inputbox.location["x"] + inputbox.size["width"] / 2,
-            512,
-            delta=40,
+            inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=40
         )
 
-        # She starts a new list and sees the input is nicely centered there too
+        # She starts a new list and sees the input is nicely
+        # centered there too
         inputbox.send_keys("testing")
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: testing")
         inputbox = self.browser.find_element_by_id("id_new_item")
         self.assertAlmostEqual(
-            inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=10
+            inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=40
         )
