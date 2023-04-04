708e4b247c6e96ebffba06acab3f08b52a99c778
nconnel4 <nconnel4@hotmail.com>
2023-04-04 19:47:48 -0400

    Fix typo in SECRET_KEY

--- superlists/settings.py
+++ superlists/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 if "DJANGO_DEBUG_FALSE" in os.environ:
     DEBUG = False
-    SECCRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
     ALLOWED_HOSTS: [os.environ["SITENAME"]]
 else:
     DEBUG = True
